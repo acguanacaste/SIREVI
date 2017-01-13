b38ba9bb30210d4c475c2699416593746dec9a92
Federico Matarrita Arata <federico@usematics.com>
2017-01-12 21:34:30 -0600

    usar el mismo header y footer del resto del sitio

--- view/menuPrincipal.php
+++ view/menuPrincipal.php
@@ -1,27 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="es">
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width= device-width, user-scale=no, initial-scale=1.0, minimum-scale=1.0">
-        <title>SIREVI Principal</title>
-        <link href="//estilos.php" rel="stylesheet" type="text/css" />
-        <script type="text/javascript" src="../assets/js/jquery.anexsoft-validator.js"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min."></script>
-    <!--  <link rel="stylesheet" type="text/css" href="assets/css/estilos.css" />
-    <!--    <link rel="stylesheet" href="assets/css/style1.css" /> -->
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.1/react-dom.min.js"/>
-        <link rel="stylesheet" href="https://cdn.rawgit.com/Dogfalo/materialize/master/dist/js/materialize.min.js"/>
-
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><!--PARA LOS ICONOS-->
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-        <!-- Compiled and minified JavaScript -->
-        <!--ME DA LA PROPIEDAD DE HACER MAS PEQUENO EL AREA DONDE ESCRIBO -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
-
-    </head>
+<?php require_once ('includes/header.php'); ?>
     <script>
     $( document ).ready(function(){
       $(".button-collapse").sideNav();
@@ -32,90 +9,7 @@
     })
 
     </script>
-    <header>
-      <div class="slider">
-         <ul class="slides">
-           <li>
-             <img src=""> <!-- Imagen tomada de la pagina oficial de el ACG -->
-             <div class="caption center-align">
-               <h3>Bienvenido Area de Conservacion Guanacaste</h3>
-               <h5 class="light grey-text ">Sistema Registro de Visitacion</h5>
-             </div>
-           </li>
-           <!--===============================================================================================================-->
-           <li>
-          <img class="background" src="assets/img/Santa_Rosa2.jpg"> <!-- random image -->
-             <div class="caption center-align">
-               <h3>Sector Santa Rosa</h3>
-               <h5 class="light grey-text text-lighten-3"><p>El Sector Santa Rosa del Área de Conservación Santa Rosa está en el corazón del bosque tropical seco,
-                  cuna de historia y tradiciones que guarda el Museo de La Casona de Santa Rosa,
-                   hogar del Centro de Investigación del ACG y las oficinas administrativas del ACG.<p></h5>
-             </div>
-           </li>
-           <!--===============================================================================================================-->
 
-           <li>
-             <img src="assets/img/pailas3.jpg"> <!-- random image -->
-             <div class="caption left-align">
-               <h3>Sector Pailas</h3>
-               <h5 ><p class="white-text text-darken-3">Sitio donde se combina el componente volcánico, con sus fumarolas, lagunas y Pailas de Barro,
-                  con la gran variedad de ecosistemas y paisajes impresionantes, en donde es posible observar el cráter activo del volcán, cataratas,
-                   zonas de pastizales en regeneración, y bosques primarios, a través de senderos 100% naturales, con muchas probabilidades de avistar aves,
-                    mamíferos y reptiles.</p></h5>
-             </div>
-           </li>
-           <!--===============================================================================================================-->
-
-           <li>
-             <img src="assets/img/junquillal3.jpg"> <!-- random image -->
-             <div class="caption left-align">
-               <h3>Refugio Nacional de Vida Silvestre Bahía Junquillal</h3>
-               <h5 class="light grey-text text-lighten-3">
-                 <p>Sitio rodeado de una gran riqueza biológica al contar con 7 diferentes ecosistemas costeros,
-                   en donde el agua está presente.
-                 La propiedad conocida como Junquillal fue donada en 1988 por la compañía Ranchos Horizontes,
-                  con el objetivo de que fuera dedicada a la conservación del medio ambiente como parte del Área de Conservación Guanacaste (ACG).
-                     Además comprende una playa de gran belleza escénica que cuenta con la categoría de Bandera Azul Ecológica con tres estrellas.
-                      Su extensión total es de 2 kilómetros de largo.</p>
-              </h5>
-             </div>
-           </li>
-           <!--======================================================================================================================-->
-           <li>
-             <img src="assets/img/sector_Murcielago.jpg"> <!-- random image -->
-             <div class="caption left-align">
-               <h3>Sector Murcielago</h3>
-               <h5 class="light grey-text text-lighten-3"><p>Sitio especial para disfrutar de la naturaleza y las características
-                  singulares que brinda el bosque seco, sus playas, sus senderos, ríos de agua dulce y manglar,
-                   así como también su importancia geológica donde se puede apreciar los cerros de la península de Santa Elena,
-                    las tierras más antiguas de Costa Rica; su flora y fauna harán de su visita motivo de satisfacción y relajación.</p></h5>
-             </div>
-           </li>
-         <!--======================================================================================================================-->
-           <li>
-             <img src="assets/img/santamaria3.jpg"> <!-- random image -->
-             <div class="caption right-align">
-               <h3>Sector Santa Maria</h3>
-               <h5><p class="white-text text-darken-3">El Sector Santa María del Área de Conservación Guanacaste,
-                  ubicado en la parte sureste del Volcán Rincón de la Vieja,
-                  es un busque húmedo donde se pueden apreciar varrias cataratas y ríos,
-                   así como unas aguas tarmales y un sendero que comunica con el Sector Pailas.<p></h5>
-             </div>
-           </li>
-           <!--=======================================================================================================================-->
-           <li>
-             <img src="assets/img/horizontes.jpg"> <!-- random image -->
-             <div class="caption center-align">
-               <h3>Estación Experimental Forestal Horizontes </h3>
-               <h5 class="light grey-text text-lighten-3"><p>En Horizontes el visitante podrá encontrar variedad de atractivos dentro del bosque seco tropical
-                  entre ellos senderos con distancias que van desde los 600 m hasta los 6 km, con diferentes niveles de dificultad,
-                   idóneos para la observación de aves, especies forestales propias de estos bosques,
-                    caminatas de aventura y práctica de ciclismo de montaña.</p></h5>
-             </div>
-           </li>
-         </ul>
-       </div>
-    </header>
     <body class="body" >
       <!--INICIO DEL side nav-->
       <!--==============================================MENU HORIZONTAL BOTON ROJA=================================================================-->
@@ -143,7 +37,90 @@
          </div>
        </nav>
       <!--======================================================================================================================-->
+      <header>
+          <div class="slider">
+              <ul class="slides">
+                  <li>
+                      <img src=""> <!-- Imagen tomada de la pagina oficial de el ACG -->
+                      <div class="caption center-align">
+                          <h3>Bienvenido Area de Conservacion Guanacaste</h3>
+                          <h5 class="light grey-text ">Sistema Registro de Visitacion</h5>
+                      </div>
+                  </li>
+                  <!--===============================================================================================================-->
+                  <li>
+                      <img class="background" src="assets/img/Santa_Rosa2.jpg"> <!-- random image -->
+                      <div class="caption center-align">
+                          <h3>Sector Santa Rosa</h3>
+                          <h5 class="light grey-text text-lighten-3"><p>El Sector Santa Rosa del Área de Conservación Santa Rosa está en el corazón del bosque tropical seco,
+                                  cuna de historia y tradiciones que guarda el Museo de La Casona de Santa Rosa,
+                                  hogar del Centro de Investigación del ACG y las oficinas administrativas del ACG.<p></h5>
+                      </div>
+                  </li>
+                  <!--===============================================================================================================-->
 
+                  <li>
+                      <img src="assets/img/pailas3.jpg"> <!-- random image -->
+                      <div class="caption left-align">
+                          <h3>Sector Pailas</h3>
+                          <h5 ><p class="white-text text-darken-3">Sitio donde se combina el componente volcánico, con sus fumarolas, lagunas y Pailas de Barro,
+                                  con la gran variedad de ecosistemas y paisajes impresionantes, en donde es posible observar el cráter activo del volcán, cataratas,
+                                  zonas de pastizales en regeneración, y bosques primarios, a través de senderos 100% naturales, con muchas probabilidades de avistar aves,
+                                  mamíferos y reptiles.</p></h5>
+                      </div>
+                  </li>
+                  <!--===============================================================================================================-->
+
+                  <li>
+                      <img src="assets/img/junquillal3.jpg"> <!-- random image -->
+                      <div class="caption left-align">
+                          <h3>Refugio Nacional de Vida Silvestre Bahía Junquillal</h3>
+                          <h5 class="light grey-text text-lighten-3">
+                              <p>Sitio rodeado de una gran riqueza biológica al contar con 7 diferentes ecosistemas costeros,
+                                  en donde el agua está presente.
+                                  La propiedad conocida como Junquillal fue donada en 1988 por la compañía Ranchos Horizontes,
+                                  con el objetivo de que fuera dedicada a la conservación del medio ambiente como parte del Área de Conservación Guanacaste (ACG).
+                                  Además comprende una playa de gran belleza escénica que cuenta con la categoría de Bandera Azul Ecológica con tres estrellas.
+                                  Su extensión total es de 2 kilómetros de largo.</p>
+                          </h5>
+                      </div>
+                  </li>
+                  <!--======================================================================================================================-->
+                  <li>
+                      <img src="assets/img/sector_Murcielago.jpg"> <!-- random image -->
+                      <div class="caption left-align">
+                          <h3>Sector Murcielago</h3>
+                          <h5 class="light grey-text text-lighten-3"><p>Sitio especial para disfrutar de la naturaleza y las características
+                                  singulares que brinda el bosque seco, sus playas, sus senderos, ríos de agua dulce y manglar,
+                                  así como también su importancia geológica donde se puede apreciar los cerros de la península de Santa Elena,
+                                  las tierras más antiguas de Costa Rica; su flora y fauna harán de su visita motivo de satisfacción y relajación.</p></h5>
+                      </div>
+                  </li>
+                  <!--======================================================================================================================-->
+                  <li>
+                      <img src="assets/img/santamaria3.jpg"> <!-- random image -->
+                      <div class="caption right-align">
+                          <h3>Sector Santa Maria</h3>
+                          <h5><p class="white-text text-darken-3">El Sector Santa María del Área de Conservación Guanacaste,
+                                  ubicado en la parte sureste del Volcán Rincón de la Vieja,
+                                  es un busque húmedo donde se pueden apreciar varrias cataratas y ríos,
+                                  así como unas aguas tarmales y un sendero que comunica con el Sector Pailas.<p></h5>
+                      </div>
+                  </li>
+                  <!--=======================================================================================================================-->
+                  <li>
+                      <img src="assets/img/horizontes.jpg"> <!-- random image -->
+                      <div class="caption center-align">
+                          <h3>Estación Experimental Forestal Horizontes </h3>
+                          <h5 class="light grey-text text-lighten-3"><p>En Horizontes el visitante podrá encontrar variedad de atractivos dentro del bosque seco tropical
+                                  entre ellos senderos con distancias que van desde los 600 m hasta los 6 km, con diferentes niveles de dificultad,
+                                  idóneos para la observación de aves, especies forestales propias de estos bosques,
+                                  caminatas de aventura y práctica de ciclismo de montaña.</p></h5>
+                      </div>
+                  </li>
+              </ul>
+          </div>
+      </header>
 
       <!--==========================================================OPCIONES PARA EL MENU LATERAL DEL RESPONSIVE-->
       <!--================================== MENU LATERAL DESPLEGABLE <> INFORMACION DENTRO DEL SIDENAV-->
@@ -403,31 +380,4 @@
      </div><!--FIN DE container-contact-->
 
 
-    <footer class="page-footer">
-         <div class="container">
-           <div class="row">
-             <div class="col l6 s12">
-               <h5 class="white-text">INFORMACION PARA EL FOOTER</h5>
-               <p class="grey-text text-lighten-4">Puedo usar columnas y otras cosas aqui para hacer mejor el footer</p>
-             </div>
-             <div class="col l4 offset-l2 s12">
-               <h5 class="white-text">Links</h5>
-               <ul>
-                 <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                 <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                 <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                 <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-               </ul>
-             </div>
-           </div>
-         </div>
-         <div style="align-items:left"class="footer-copyright">
-           <div style="align=left"class="container">
-           © 2016 Copyright<br> Sistema Registro Visitacion, Area Conservacion Guanacaste
-           <a class="grey-text text-lighten-4 right" href="#!">Mas Links</a>
-           </div>
-         </div>
-       </footer>
-
-  </body>
-</html>
+<?php require_once ('includes/footer.php'); ?>
