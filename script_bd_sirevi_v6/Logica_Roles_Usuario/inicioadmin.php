2c1d4d9d7ac28a343d7aa73f2ff9a9e1c8c1928f
AlexMLuna <cheeto12@gmail.com>
2017-07-14 19:10:21 -0600

    Working th logic for user rol, the logic works now needs to be implemented on sirevi system

---
+++ script_bd_sirevi_v6/Logica_Roles_Usuario/inicioadmin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Adminiatrador</title>
+  </head>
+  <body>
+    <section>
+      <center>
+        <h2>Bienvenido administrador</h2>
+      </center>
+    </section>
+  </body>
+</html>
---
+++ script_bd_sirevi_v6/Logica_Roles_Usuario/iniciousu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Usuarios</title>
+  </head>
+  <body>
+    <section>
+      <center>
+        <h2>Bievenido usuario</h2>
+    </center>
+    </section>
+
+  </body>
+</html>
