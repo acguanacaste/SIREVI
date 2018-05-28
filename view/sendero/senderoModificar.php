2b81f599a26fb38407b44de8cbad0011ebadc4f5
AlexMLuna <cheeto12@gmail.com>
2018-05-28 11:43:41 -0600

    Visual changes on Senderos page, the changes were made to make Modificar Informacion and Registro Senderos could match on large and medium screen

--- view/sendero/senderoModificar.php
+++ view/sendero/senderoModificar.php
@@ -70,20 +70,20 @@
 
             <!---Inicio de la tercera fila *columna1-->
               <div class="">
-                <div class="input-field col s12 m6 l4  ">
+                <div class="input-field col s12 m4 l4  ">
                   <input  id="distancia" type="text" name="distancia" value="<?php echo $sendero->distancia; ?>" class="validate" class="form-control" required >
                   <label for="distancia" >  <i class="small material-icons">transfer_within_a_station</i>Distancia ida y vuelta</label>
              </div>
 
              <!--Columna2 fila3-->
-             <div class="input-field col s12 m6 l4  ">
+             <div class="input-field col s12 m4 l4  ">
                <input  id="tiempo_recorrido" type="text" name="tiempo_recorrido" value="<?php echo $sendero->tiempo_recorrido; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
                <label for="tiempo_recorrido" >  <i class="small material-icons">timer</i>Tiempo de recorrido</label>
               </div>
 
                <!--Columna3 fila3-->
-               <div class="input-field col s12 m6 l4  ">
-                 <input  id="" type="text" name="punto_alto" value="<?php echo $sendero->punto_alto; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+               <div class="input-field col s12 m4 l4  ">
+                 <input  id="" type="text" name="punto_alto" value="<?php echo $sendero->punto_alto; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" >
                  <label for="punto_alto" >  <i class="small material-icons">wallpaper</i>Punto mas alto</label>
                 </div>
               </div><!--Fin de la segunda fila -->
@@ -91,48 +91,43 @@
           <!---Inicio de la cuarta fila-->
               <div class="">
                 <div class="input-field col s6 m6 l6  ">
-                  <input  id="punto_inicio" type="text" name="punto_inicio" value="<?php echo $sendero->punto_inicio; ?>"  class="form-control validate" data-validacion-tipo="requerido|min:10" required >
+                  <input  id="punto_inicio" type="text" value="<?php echo $sendero->punto_inicio; ?>" name="punto_inicio"  class="form-control validate" data-validacion-tipo="requerido|min:10" >
                   <label for="punto_inicio" >  <i class="small material-icons">directions_walk</i>Punto inicio</label>
              </div>
+             <div class="input-field col s6 m6 l6  ">
+               <input  id="punto_llegada" type="text" name="punto_llegada" value="<?php echo $sendero->punto_llegada; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+               <label for="punto_llegada" >  <i class="small material-icons">directions_walk</i>Punto llegada</label>
+              </div>
+            </div>
+      <!--Fin de la cuarta fila -->
 
-             <!--columna-->
-               <div class="input-field col s6 m6 l6  ">
-                 <input  id="punto_llegada" type="text" name="punto_llegada" value="<?php echo $sendero->punto_llegada; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
-                 <label for="punto_llegada" >  <i class="small material-icons">directions_walk</i>Punto llegada</label>
-                </div>
-              </div><!--Fin de la cuarta fila -->
+      <!--Inicio de la quinta fila-->
+      <div class="">
+      <!--columna-->
+      <div class="input-field col s12 m12 l12  ">
+      <input  id="descripcion" type="text" name="descripcion" value="<?php echo $sendero->descripcion; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+      <label for="descripcion" >  <i class="small material-icons">info_outline</i>Descripción del sendero</label>
+      </div>
+      </div><!--Fin de la quinta fila -->
 
-              <!--Inicio de la quinta fila-->
-              <div class="">
-              <!--columna-->
-               <div class="input-field col s12 m12 l12  ">
-                 <input  id="descripcion" type="text" name="descripcion" value="<?php echo $sendero->descripcion; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
-                 <label for="descripcion" >  <i class="small material-icons">info_outline</i>Descripción del sendero</label>
-                </div>
-              </div><!--Fin de la quinta fila -->
+      <!--Inicio de la quinta fila-->
+      <div class="">
+      <!--columna-->
+      <div class="input-field col s12 m12 l12  ">
+      <input  id="regulaciones" type="text" name="regulaciones" value="<?php echo $sendero->regulaciones; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+      <label for="regulaciones" >  <i class="small material-icons">report_problem</i>Regulaciones</label>
+      </div>
+      </div><!--Fin de la quinta fila -->
 
-              <!--Inicio de la quinta fila-->
-              <div class="">
-              <!--columna-->
-               <div class="input-field col s12 m12 l12  ">
-                 <input  id="regulaciones" type="text" name="regulaciones" value="<?php echo $sendero->regulaciones; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
-                 <label for="regulaciones" >  <i class="small material-icons">report_problem</i>Regulaciones</label>
-                </div>
-              </div><!--Fin de la quinta fila -->
-
+      </div>
 
               <!--BOTON QUE ME ENVIA EL FORMULARIO-->
+            <center>
               <button title="Enviar" class="btn waves-effect waves-light teal darken-4"
                 value="enviar"  type="submit" name="action"><span class="hide-on-small-only">Enviar</span>
                      <i class="mdi-content-send material-icons right">done</i>
               </button>
-
-            <!--BOTON QUE ME BORRA LO QUE ESCRIBI EN EL FORMULARIO-->
-            <button title="Limpiar Pnatalla" class="btn waves-effect waves-light teal darken-4"
-              value="reset"  type="reset" name="action"><span class="hide-on-small-only">Limpiar</span>
-                   <i class="mdi-content-send material-icons right">delete</i>
-            </button>
-
+            </center>
 					 </div>
          </form>
            </div>
@@ -140,13 +135,15 @@
          </div>
        </div>
 </fieldset>
-
+<div class="right hide-on-large-only">
+  <a href="index.php?c=Sendero"><i style="color:#00b0ff" class="small material-icons blue-grey darken-2 z-depth-1 btn-floating pulse">arrow_back</i>&nbsp;Página anterior</a>
+</div>
 
 <!--================================================================================================================================-->
         </div>
       </div><!-- Div de los tamanos -->
 
-      <div class="col hide-on-small-only m3 l2">
+      <div class="hide-on-med-and-down input-field col l2">
         <div class="toc-wrapper pin-top" style="top: -15px;">
           <div class="buysellads hide-on-small-only">
             <!-- CarbonAds Zone Code -->
--- view/sendero/senderoRegistro.php
+++ view/sendero/senderoRegistro.php
@@ -68,22 +68,21 @@
                  </div>
             </div>
 
-
             <!---Inicio de la tercera fila *columna1-->
               <div class="">
-                <div class="input-field col s12 m6 l4  ">
+                <div class="input-field col s12 m4 l4  ">
                   <input  id="distancia" type="text" name="distancia" value="" class="validate" class="form-control" required >
-                  <label for="distancia" >  <i class="small material-icons">transfer_within_a_station</i>Distancia ida y vuelta</label>
+                <label for="distancia" >  <i class="small material-icons">transfer_within_a_station</i>Distancia ida y vuelta</label>
              </div>
 
              <!--Columna2 fila3-->
-             <div class="input-field col s12 m6 l4  ">
+             <div class="input-field col s12 m4 l4  ">
                <input  id="tiempo_recorrido" type="text" name="tiempo_recorrido" value="" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
                <label for="tiempo_recorrido" >  <i class="small material-icons">timer</i>Tiempo de recorrido</label>
               </div>
 
                <!--Columna3 fila3-->
-               <div class="input-field col s12 m6 l4  ">
+               <div class="input-field col s12 m4 l4  ">
                  <input  id="" type="text" name="punto_alto" value="" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" >
                  <label for="punto_alto" >  <i class="small material-icons">wallpaper</i>Punto mas alto</label>
                 </div>
@@ -95,34 +94,33 @@
                   <input  id="punto_inicio" type="text" name="punto_inicio"  class="form-control validate" data-validacion-tipo="requerido|min:10" >
                   <label for="punto_inicio" >  <i class="small material-icons">directions_walk</i>Punto inicio</label>
              </div>
+             <div class="input-field col s6 m6 l6  ">
+               <input  id="punto_llegada" type="text" name="punto_llegada" value="<?php echo $sendero->punto_llegada; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+               <label for="punto_llegada" >  <i class="small material-icons">directions_walk</i>Punto llegada</label>
+              </div>
+            </div>
+          <!--Fin de la cuarta fila -->
 
+             <!--Inicio de la quinta fila-->
+             <div class="">
              <!--columna-->
-               <div class="input-field col s6 m6 l6  ">
-                 <input  id="punto_llegada" type="text" name="punto_llegada" value="" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" >
-                 <label for="punto_llegada" >  <i class="small material-icons">directions_walk</i>Punto llegada</label>
-                </div>
-              </div><!--Fin de la cuarta fila -->
+              <div class="input-field col s12 m12 l12  ">
+                <input  id="descripcion" type="text" name="descripcion" value="<?php echo $sendero->descripcion; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+                <label for="descripcion" >  <i class="small material-icons">info_outline</i>Descripción del sendero</label>
+               </div>
+             </div><!--Fin de la quinta fila -->
 
-              <!--Inicio de la quinta fila-->
-              <div class="">
-              <!--columna-->
-               <div class="input-field col s12 m12 l12  ">
-                 <input  id="descripcion" type="text" name="descripcion" value="" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
-                 <label for="descripcion" >  <i class="small material-icons">info_outline</i>Descripción del sendero</label>
-                </div>
-              </div><!--Fin de la quinta fila -->
-
-              <!--Inicio de la quinta fila-->
-              <div class="">
-              <!--columna-->
-               <div class="input-field col s12 m12 l12  ">
-                 <input  id="regulaciones" type="text" name="regulaciones" value="" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" >
-                 <label for="regulaciones" >  <i class="small material-icons">report_problem</i>Regulaciones</label>
-                </div>
-              </div><!--Fin de la quinta fila -->
-
-
+             <!--Inicio de la quinta fila-->
+             <div class="">
+             <!--columna-->
+              <div class="input-field col s12 m12 l12  ">
+                <input  id="regulaciones" type="text" name="regulaciones" value="<?php echo $sendero->regulaciones; ?>" class="validate" class="form-control" data-validacion-tipo="requerido|min:10" required >
+                <label for="regulaciones" >  <i class="small material-icons">report_problem</i>Regulaciones</label>
+               </div>
+             </div><!--Fin de la quinta fila -->
+           </div>
               <!--BOTON QUE ME ENVIA EL FORMULARIO-->
+              <center>
               <button title="Enviar" class="btn waves-effect waves-light teal darken-4"
                 value="enviar"  type="submit" name="action"><span class="hide-on-small-only">Enviar</span>
                      <i class="mdi-content-send material-icons right">done</i>
@@ -132,22 +130,22 @@
             <button title="Limpiar Pnatalla" class="btn waves-effect waves-light teal darken-4"
               value="reset"  type="reset" name="action"><span class="hide-on-small-only">Limpiar</span>
                    <i class="mdi-content-send material-icons right">delete</i>
-            </button>
-
-					 </div>
-         </form>
-           </div>
-
-         </div>
-       </div>
-</fieldset>
-
+              </button>
+            </center>
+          </form>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+<div class="right hide-on-large-only">
+  <a href="index.php?c=Sendero"><i style="color:#00b0ff" class="small material-icons blue-grey darken-2 z-depth-1 btn-floating pulse">arrow_back</i>&nbsp;Página anterior</a>
+</div>
 
 <!--================================================================================================================================-->
         </div>
       </div><!-- Div de los tamanos -->
 
-      <div class="col hide-on-small-only m3 l2">
+      <div class="input-field col l2 hide-on-med-and-down">
         <div class="toc-wrapper pin-top" style="top: -15px;">
           <div class="buysellads hide-on-small-only">
             <!-- CarbonAds Zone Code -->
